95e15a59ff95c781ffe4a69182996d2769d09a61
houdamzari <houdamzari1999@gmail.com>
2023-02-22 11:21:10 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -29,3 +29,101 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 
+-- Begin the transaction
+BEGIN;
+
+-- Update the species column of all animals to 'unspecified'
+UPDATE animals SET species = 'unspecified';
+
+-- Verify that the change was made
+SELECT * FROM animals;
+
+-- Roll back the transaction
+ROLLBACK;
+
+-- Verify that the species column went back to its state before the transaction
+SELECT * FROM animals;
+
+-- Begin the transaction
+BEGIN;
+
+-- Update the species column to 'digimon' for all animals whose name ends in 'mon'
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the species column to 'pokemon' for all animals whose species is not set
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+-- Commit the transaction
+COMMIT;
+
+-- Verify that the changes were made and persist after commit
+SELECT * FROM animals;
+
+-- Begin the transaction
+BEGIN;
+
+-- Update the species column of all animals to 'unspecified'
+UPDATE animals SET species = 'unspecified';
+
+-- Verify that the change was made
+SELECT * FROM animals;
+
+-- Roll back the transaction
+ROLLBACK;
+
+-- Verify that the species column went back to its state before the transaction
+SELECT * FROM animals;
+
+
+-- Begin the transaction
+BEGIN;
+
+-- Update the species column to 'digimon' for all animals whose name ends in 'mon'
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the species column to 'pokemon' for all animals whose species is not set
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+-- Commit the transaction
+COMMIT;
+
+-- Verify that the changes were made and persist after commit
+SELECT * FROM animals;
+
+-- SQL code to delete all records in the animals table inside a transaction and then roll back the transaction
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+    -- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+    -- How many animals have never tried to escape?
+   SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+    -- What is the average weight of animals?
+    SELECT AVG(weight_kg) FROM animals;
+
+        -- Who escapes the most, neutered or not neutered animals?
+        SELECT neutered, SUM(escape_attempts) as total_escapes 
+FROM animals 
+GROUP BY neutered 
+ORDER BY total_escapes DESC 
+LIMIT 1;
+
+    -- What is the minimum and maximum weight of each type of animal?
+    SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight 
+FROM animals 
+GROUP BY species;
+
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) as avg_escape_attempts 
+FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' 
+GROUP BY species;
